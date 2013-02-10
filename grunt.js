4421bd2812ce198c565f4909fd0085543079c48e
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-02-10 11:41:51 -0500

    Updating banner alignment

--- grunt.js
+++ grunt.js
@@ -3,16 +3,16 @@ module.exports = function (grunt) {
 		pkg : "<json:package.json>",
 		meta : {
 			banner : "/**\n" + 
-					 " * <%= pkg.name %>\n" +
-					 " *\n" +
-					 " * <%= pkg.description %>\n" +
-					 " *\n" +
-					 " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
-					 " * @copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
-					 " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
-					 " * @link <%= pkg.homepage %>\n" +
-					 " * @version <%= pkg.version %>\n" +
-					 " */"
+			         " * <%= pkg.name %>\n" +
+			         " *\n" +
+			         " * <%= pkg.description %>\n" +
+			         " *\n" +
+			         " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
+			         " * @copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
+			         " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
+			         " * @link <%= pkg.homepage %>\n" +
+			         " * @version <%= pkg.version %>\n" +
+			         " */"
 		},
 		concat: {
 		  dist: {
