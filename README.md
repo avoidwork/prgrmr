e36c77b4e72df6ec1259e1f55611afa5b3e0d37f
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-02-10 11:39:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# prgrmr
+
+Programmer profile for GitHub projects
+
+## Documentation
+_(Coming soon)_
+
+## License
+Copyright (c) 2013 Jason Mulligan  
+Licensed under the BSD-3 license.
---
+++ grunt.js
@@ -0,0 +1,44 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg : "<json:package.json>",
+		meta : {
+			banner : "/**\n" + 
+					 " * <%= pkg.name %>\n" +
+					 " *\n" +
+					 " * <%= pkg.description %>\n" +
+					 " *\n" +
+					 " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
+					 " * @copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
+					 " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
+					 " * @link <%= pkg.homepage %>\n" +
+					 " * @version <%= pkg.version %>\n" +
+					 " */"
+		},
+		concat: {
+		  dist: {
+			src : [
+			  "<banner>",
+			  "src/intro.js",
+			  "src/outro.js"
+			],
+			dest : "lib/prgrmr.js"
+		  }
+		},
+		test : {
+		  files : ["test/**/*.js"]
+		}
+	});
+
+	// Replaces occurrances of {{VERSION}} with the value from package.json
+	grunt.registerTask("version", function () {
+		var ver = grunt.config("pkg").version,
+		    fn  = grunt.config("concat").dist.dest,
+		    fp  = grunt.file.read(fn);
+
+		console.log("Setting version to: " + ver);
+		grunt.file.write(fn, fp.replace(/\{\{VERSION\}\}/g, ver));
+	});
+
+	// Concatting, setting version & testing
+	grunt.registerTask("default", "concat version test");
+};
