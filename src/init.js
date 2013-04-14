3db3e50d6efe830b7994358619bf2b9ee4eef098
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-04-14 08:12:21 -0400

    Fixing an erroneous statement that used to rely on `typeof` operator, spacing out statements for easier debugging, removing `turtle.io` as a dev dependency (no reason it'd be one)

--- src/init.js
+++ src/init.js
@@ -15,7 +15,9 @@ var init = function () {
 	window.humane.error = window.humane.spawn({addnCls: "humane-jackedup-error", timeout: 3000});
 
 	// Decorating placeholders
-	if (version !== "undefined") version.html(prgrmr.version);
+	if (version !== undefined) {
+		version.html(prgrmr.version);
+	}
 
 	// Adding a spinner
 	loading = spinner(main, "large");
@@ -23,7 +25,9 @@ var init = function () {
 	// Retrieving the config
 	"assets/config.json".get(function (config) {
 		// Did you edit config.json?
-		if (config.github.isEmpty()) throw Error($.label.error.invalidArguments);
+		if (config.github.isEmpty()) {
+			throw Error($.label.error.invalidArguments);
+		}
 
 		// Setting config on namespace
 		prgrmr.config = config;
@@ -46,6 +50,7 @@ var init = function () {
 			loading.el.destroy();
 			loading = null;
 		}
+
 		error("Configuration is not valid: " + (e.message || e));
 		throw e;
 	}).then(function (config) {
@@ -63,11 +68,26 @@ var init = function () {
 
 			// Showing contact icons
 			contact.create("li").create("a", {"class": "github", href: "https://github.com/" + config.github, title: "GitHub"}).create("span", {"class": "icon icon-github"});
-			if (config.email && rec.data.email !== null) contact.create("li").create("a", {"class": "email", href: "mailto:" + rec.data.email, title: "Email"}).create("span", {"class": "icon icon-envelope-alt"});
-			if (!config.gplus.isEmpty()) contact.create("li").create("a", {"class": "gplus", href: "https://plus.google.com/" + config.gplus, title: "Google Plus"}).create("span", {"class": "icon icon-google-plus"});
-			if (!config.twitter.isEmpty()) contact.create("li").create("a", {"class": "twitter", href: "http://twitter.com/" + config.twitter, title: "Twitter"}).create("span", {"class": "icon icon-twitter"});
-			if (!config.linkedin.isEmpty()) contact.create("li").create("a", {"class": "linkedin", href: config.linkedin, title: "LinkedIn"}).create("span", {"class": "icon icon-linkedin"});
-			if (config.blog && rec.data.blog !== null) contact.create("li").create("a", {"class": "blog", href: rec.data.blog, title: "Blog"}).create("span", {"class": "icon icon-rss"});
+
+			if (config.email && rec.data.email !== null) {
+				contact.create("li").create("a", {"class": "email", href: "mailto:" + rec.data.email, title: "Email"}).create("span", {"class": "icon icon-envelope-alt"});
+			}
+
+			if (!config.gplus.isEmpty()) {
+				contact.create("li").create("a", {"class": "gplus", href: "https://plus.google.com/" + config.gplus, title: "Google Plus"}).create("span", {"class": "icon icon-google-plus"});
+			}
+
+			if (!config.twitter.isEmpty()) {
+				contact.create("li").create("a", {"class": "twitter", href: "http://twitter.com/" + config.twitter, title: "Twitter"}).create("span", {"class": "icon icon-twitter"});
+			}
+
+			if (!config.linkedin.isEmpty()) {
+				contact.create("li").create("a", {"class": "linkedin", href: config.linkedin, title: "LinkedIn"}).create("span", {"class": "icon icon-linkedin"});
+			}
+
+			if (config.blog && rec.data.blog !== null) {
+				contact.create("li").create("a", {"class": "blog", href: rec.data.blog, title: "Blog"}).create("span", {"class": "icon icon-rss"});
+			}
 
 			contact.parentNode.removeClass("hidden");
 
@@ -77,8 +97,12 @@ var init = function () {
 
 			// Retrieving data
 			["events", "orgs", "repos"].forEach(function (i) {
-				try { prgrmr[i].data.restore(); }
-				catch (e) { void 0; }
+				try {
+					prgrmr[i].data.restore();
+				}
+				catch (e) {
+					void 0;
+				}
 
 				if (prgrmr[i].data.total > 0) {
 					prepare(i).then(function () {
